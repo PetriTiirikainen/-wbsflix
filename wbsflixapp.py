09f6761b50f5223fa510bede9f96e9300a1a0e8e
Petri Tiirikainen <147138344+PetriTiirikainen@users.noreply.github.com>
2024-02-15 10:57:32 +0100

    Update wbsflixapp.py
    V2, better functionality and new layout

--- wbsflixapp.py
+++ wbsflixapp.py
@@ -3,6 +3,7 @@ import pandas as pd
 from func_top_n_movies import top_n_movies
 from func_get_item_based_movies import get_movie_recommendations
 from func_get_user_based_movies import recommend_movies
+from func_get_user_based_movies import recommend_movies_by_genre
 from tmdb_api import get_poster_url
 from user_input import user_input_features
 from displayposters import display_posters
@@ -20,7 +21,7 @@ st.markdown(
         """
         <style>
         .logo-text {
-            font-size:50px !important;
+            font-size:80px !important;
             color: mediumseagreen !important;
             position: absolute;
             top: 2%;
@@ -32,59 +33,75 @@ st.markdown(
         """, unsafe_allow_html=True
 )
 
-    # Add line breaks
+# Add line breaks
+st.markdown("<br>", unsafe_allow_html=True)
+st.markdown("<br>", unsafe_allow_html=True)
+st.markdown("<br>", unsafe_allow_html=True)
 st.markdown("<br>", unsafe_allow_html=True)
 st.markdown("<br>", unsafe_allow_html=True)
 
-    # Chatbot's header
+# User inputs
+movie_name = st.text_input('Search for a movie', key='movie_name')
+user_id = st.text_input('Enter your user ID please', '42', key='user_id')
+user_id = int(user_id)  # Convert the user ID to an integer
+top_n = 10  # Define the number of recommendations to display
+
+# Chatbot's header
 st.header("Movie Recommendation Chatbot")
 
-    # Chatbot's greeting message
+# Chatbot's greeting message
 st.write("Hello, how can I help you today? Try entering a movie or a genre you liked or your user ID.")
 
-    # Create a sidebar for user inputs
-st.sidebar.header('Control Panel')
+# Use st.text_input for chatbot input
+chatbot_input = st.text_input('Chatbot Input', help='Enter your user ID or a movie or a genre you liked', key='chatbot_input')
 
-movie_name, user_id, top_n = user_input_features()
-
-    # Use st.text_input for chatbot input
-chatbot_input = st.text_input('Chatbot Input', help='Enter your user ID or a movie or a genre you liked')
+# Define the variable "genre" before using it
+genre = None  
 
 if chatbot_input:
-        if chatbot_input.lower() == "banana!":
+    if chatbot_input.lower() == "banana!":
             # Easter egg for 'banana!'
-            st.write("Maxguv minion! aca nama tadda ka kaylay cama to: (Welcome minion! This is all I have of you:)")
+            st.write("Maxguv minion! Aca nama tadda ka kaylay cama to: (Translation: Welcome minion! This is all I have of you:)")
             minion_movies = movies[movies['title'].str.lower().str.contains('despicable me') | movies['title'].str.lower().str.contains('minions')]
             minion_movies = minion_movies.merge(links, on='movieId')  # Merge with links DataFrame
             if not minion_movies.empty:
                 display_posters(minion_movies)
             else:
                 st.write("Sorry, no Minions movies found.")
-
-        elif chatbot_input.isdigit():
+    elif chatbot_input.isdigit():
             # If the user input is a number, assume it's a user ID and recommend a movie
             user_id = int(chatbot_input)
             recommendations = recommend_movies(user_id, ratings, movies, links, 1)
             # Use st.write for chatbot output
             st.write(f"I recommend the movie: {recommendations['title'].values[0]}")
             display_posters(recommendations)
-        elif any(chatbot_input.lower() in movie.lower() for movie in movies['title'].values):
-            # If the user input is a substring of a movie title, recommend a similar movie
-            movie_title = next(movie for movie in movies['title'].values if chatbot_input.lower() in movie.lower())
-            recommendations = get_movie_recommendations(movie_title, 1, movies, tags, links)
-            st.write(f"If you liked {movie_title}, you might also like: {recommendations['title'].values[0]}")
+    elif any(chatbot_input.lower() in genre.lower() for genre in ['Adventure', 'Animation', 'Children', 'Comedy', 'Fantasy', 'Romance', 'Drama',
+    'Action', 'Crime', 'Thriller', 'Horror', 'Mystery', 'Sci-Fi', 'War', 'Musical',
+    'Documentary', 'IMAX', 'Western', 'Film-Noir']):
+        # If the user input is a genre, recommend a movie from that genre
+        genre = next(genre for genre in ['Adventure', 'Animation', 'Children', 'Comedy', 'Fantasy', 'Romance', 'Drama',
+        'Action', 'Crime', 'Thriller', 'Horror', 'Mystery', 'Sci-Fi', 'War', 'Musical',
+        'Documentary', 'IMAX', 'Western', 'Film-Noir'] if chatbot_input.lower() in genre.lower())
+        recommendations = recommend_movies_by_genre(genre, movies, links, 1)
+        if not recommendations.empty:
+            st.write(f"I recommend the movie: {recommendations['title'].values[0]}")
             display_posters(recommendations)
-        else:
-            st.write("Sorry, I didn't understand that. Please try again.")
+    elif any(chatbot_input.lower() in movie.lower() for movie in movies['title'].values):
+        # If the user input is a substring of a movie title, recommend a similar movie
+        movie_title = next(movie for movie in movies['title'].values if chatbot_input.lower() in movie.lower())
+        recommendations = get_movie_recommendations(movie_title, 1, movies, tags, links)
+        st.write(f"If you liked {movie_title}, you might also like: {recommendations['title'].values[0]}")
+        display_posters(recommendations)
+else:
+    st.write("Sorry, I didn't understand that. Please try again.")
 
-
-    # Merge the 'movies' and 'links' DataFrames on 'movieId'
+# Merge the 'movies' and 'links' DataFrames on 'movieId'
 movies_with_links = movies.merge(links, on='movieId')
 
-    # Filter the merged DataFrame for titles that contain the user input
+# Filter the merged DataFrame for titles that contain the user input
 search_results = movies_with_links[movies_with_links['title'].str.lower().str.contains(movie_name.lower())]
 
-    # If the user inputs something in the search field, filter the merged DataFrame for titles that contain the user input and display the results
+# If the user inputs something in the search field, filter the merged DataFrame for titles that contain the user input and display the results
 if movie_name:
         search_results = movies_with_links[movies_with_links['title'].str.lower().str.contains(movie_name.lower())]
 
@@ -96,26 +113,26 @@ if movie_name:
             st.subheader(f"Search results for '{movie_name}':")
             display_posters(search_results)
 
-    # Display top n movies
+# Display top n movies
 st.subheader("Our customers' favourites")
 top_movies = top_n_movies(top_n, movies, ratings, links)  # Add links as an argument
 display_posters(top_movies)
 
-    # Filter the ratings DataFrame for rows where the userId is the specified user and the rating is 4 or 5
+# Filter the ratings DataFrame for rows where the userId is the specified user and the rating is 4 or 5
 high_rated_movies = ratings[(ratings['userId'] == user_id) & (ratings['rating'] >= 4)]
 
-    # Randomly select one of these high-rated movies
+# Randomly select one of these high-rated movies
 random_high_rated_movie = high_rated_movies.sample(1)
 
-    # Get the title of the randomly selected high-rated movie from the movies DataFrame
+# Get the title of the randomly selected high-rated movie from the movies DataFrame
 random_high_rated_movie_title = movies[movies['movieId'] == random_high_rated_movie['movieId'].values[0]]['title'].values[0]
 
-    # Display item-based recommendations
+# Display item-based recommendations
 st.subheader(f"Because you liked '{random_high_rated_movie_title}', you might also like...")
 item_recommendations = get_movie_recommendations(random_high_rated_movie_title, top_n, movies, tags, links)  # Add links as an argument
 display_posters(item_recommendations)
 
-    # Display user-based recommendations
+# Display user-based recommendations
 st.subheader('Specially for you')
 user_recommendations = recommend_movies(user_id, ratings, movies, links, top_n)  # Add links as an argument
 display_posters(user_recommendations)
